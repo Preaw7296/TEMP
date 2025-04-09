1c1e468717a2df6726df2b3af9993943d92e6e25
Preaw7296 <152893588+Preaw7296@users.noreply.github.com>
2025-04-09 09:57:11 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-console.log(process.env.MONGO_URI); 
+const mongoURI = process.env.MONGO_URI;
 
 
 mongoose.connect(process.env.MONGO_URI, {
